20e626f52c85d55d1bd3863149b2973a633b783d
Nikolas-vl <nikvv02@gmail.com>
2025-04-07 17:31:07 +0200

    done 1 task

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,5 +1,17 @@
-const TodoList = () => {
-  return <h3>TodoList</h3>;
+import Grid from '../Grid/Grid';
+import GridItem from '../GridItem/GridItem';
+import TodoListItem from '../TodoListItem/TodoListItem';
+
+const TodoList = ({ todos, onDelete }) => {
+  return (
+    <Grid>
+      {todos.map((todo, index) => (
+        <GridItem key={todo.id}>
+          <TodoListItem number={index + 1} todo={todo} onDelete={onDelete} />
+        </GridItem>
+      ))}
+    </Grid>
+  );
 };
 
 export default TodoList;
--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -1,9 +1,55 @@
+import { useState, useEffect } from 'react';
+import Form from '../components/Form/Form';
 import Text from '../components/Text/Text';
+import TodoList from '../components/TodoList/TodoList';
+import { nanoid } from 'nanoid';
 
 const Todos = () => {
+  const [todos, setTodos] = useState(() => {
+    const savedTodos = localStorage.getItem('todos');
+    // Перевіряємо, чи є дані та чи вони не є "undefined"
+    if (savedTodos && savedTodos !== 'undefined') {
+      try {
+        return JSON.parse(savedTodos); // Перетворюємо збережене на об'єкт
+      } catch (error) {
+        console.error('Error parsing todos from localStorage:', error);
+        return []; // Повертаємо порожній масив, якщо виникла помилка
+      }
+    }
+    return []; // Якщо дані відсутні або "undefined", повертаємо порожній масив
+  });
+
+  // Функція для додавання нової тудушки
+  const addNewTodo = text => {
+    const newTodo = {
+      text,
+      id: nanoid(),
+    };
+    setTodos(prevTodos => {
+      const updatedTodos = [...prevTodos, newTodo];
+      localStorage.setItem('todos', JSON.stringify(updatedTodos)); // Оновлюємо localStorage
+      return updatedTodos;
+    });
+  };
+
+  // Функція для видалення тудушки
+  const deleteTodo = id => {
+    setTodos(prevTodos => {
+      const updatedTodos = prevTodos.filter(todo => todo.id !== id);
+      localStorage.setItem('todos', JSON.stringify(updatedTodos)); // Оновлюємо localStorage
+      return updatedTodos;
+    });
+  };
+
   return (
     <>
-      <Text textAlign="center">There are no any todos ...</Text>
+      <Form onSubmit={addNewTodo} />
+
+      {todos.length === 0 && (
+        <Text textAlign="center">There are no any todos ...</Text>
+      )}
+
+      <TodoList todos={todos} onDelete={deleteTodo} />
     </>
   );
 };
